70a94f5358f0b5b70dafa877eab2ad4da05efdaa
parth sharma <ps13092007@gmail.com>
2025-06-02 13:02:52 +0530

    page err fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route , Router as WouterRouter } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
@@ -15,17 +15,19 @@ import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
-    <Layout>
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/search" component={Search} />
-        <Route path="/properties" component={Properties} />
-        <Route path="/property/:id" component={PropertyDetail} />
-        <Route path="/agent" component={Agent} />
-        <Route path="/contact" component={Contact} />
-        <Route component={NotFound} />
-      </Switch>
-    </Layout>
+    <WouterRouter base="/dreamghhar">
+      <Layout>
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/search" component={Search} />
+          <Route path="/properties" component={Properties} />
+          <Route path="/property/:id" component={PropertyDetail} />
+          <Route path="/agent" component={Agent} />
+          <Route path="/contact" component={Contact} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
+    </WouterRouter>
   );
 }
 
